33ec4fc48333b4954158a9b92b653d3ca972e207
sm-svyat <sm.svyat@yandex.ru>
2017-10-23 08:32:41 +0300

    Added simplified chat

--- tkilla_client.py
+++ tkilla_client.py
@@ -1,33 +1,94 @@
+import sys
 import socket
 import json
 import time
+from jimprotocols import JimMessage
 
-def connection(json_object):
+#def connection(json_object):
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('localhost', 8888))
-    sock.send(json_object.encode('utf-8'))
-
-
-    data = sock.recv(1024)
-    sock.close()
-    return data
-
-authenticate_request = {
-    "action": "authenticate",
-    "time": "<unix timestamp>",
-    "user": {
-    "account_name": "C0deMaver1ck",
-    "password": "CorrectHorseBatteryStaple"
-    }
-    }
-
-authenticate_request['time'] = time.ctime()
-request = json.dumps(authenticate_request)
-data = connection(request)
+#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    sock.connect(('localhost', 8888))
+#    sock.send(json_object.encode('utf-8'))
 
 
-json_data = json.loads(data.decode('utf-8'))
-print(json_data['response'])
-if json_data['response'] // 100 == 4:
-    print(json_data['error'])
+#    data = sock.recv(1024)
+#    sock.close()
+#    return data
+
+class Conversation:
+    def __init__(self, host='localhost', port=8888):
+        self.host = host
+        self.port = port
+        self.sock = object()
+        self.chat = list()
+
+    def connection(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, self.port))
+
+
+    def readmsg(self):
+        #self.connection()
+        self.data = self.sock.recv(1024)
+        self.chat.append(json.loads(self.data.decode('utf-8')))
+
+    def writemsg(self, json_object):
+        #self.connection()
+        self.chat.append(json_object)
+        self.sock.send(json_object)
+
+
+
+if __name__ == "__main__":
+
+#            authenticate_request = {
+#            "action": "authenticate",
+#            "time": "<unix timestamp>",
+#            "user": {
+#                "account_name": "C0deMaver1ck",
+#                "password": "CorrectHorseBatteryStaple"
+#            }
+#        }
+
+#        authenticate_request['time'] = time.ctime()
+#        request = json.dumps(authenticate_request)
+#        data = connection(request)
+
+#        json_data = json.loads(data.decode('utf-8'))
+#        print(json_data['response'])
+#        if json_data['response'] // 100 == 4:
+#            print(json_data['error'])
+
+    try:
+        mode = sys.argv[1]
+        if mode not in ('r', 'w'):
+            print('Режим должен быть r - чтение, w - запись')
+            sys.exit(0)
+    except IndexError:
+        mode = 'r'
+
+    my_chat = Conversation()
+    my_chat.connection()
+
+    if mode == 'w':
+        json_msg_teg = json.dumps({'action': 'msg', 'mode': 'w'}).encode('utf-8')
+        my_chat.writemsg(json_msg_teg)
+        while True:
+            msg = input(':) >')
+            my_chat.connection()
+            msg = JimMessage('Me', 'You', msg)
+            data = msg.jsonmsg()
+            my_chat.writemsg(data)
+
+
+    elif mode == 'r':
+        json_msg_teg = json.dumps({'action': 'msg', 'mode': 'r'}).encode('utf-8')
+        my_chat.writemsg(json_msg_teg)
+        while True:
+            my_chat.readmsg()
+            msg = my_chat.chat[-1]
+            #print('Здесь сообщение ', msg)
+            print('{} \n{:>80} \n{:>80}\n'.format(msg['message'], msg['from'], msg['time']))
+
+    else:
+        print('Неверный режим запуска {}. Режим запуска должен быть r - чтение или w - запись'.format(mode))
--- tkilla_server.py
+++ tkilla_server.py
@@ -1,6 +1,8 @@
 import socket
 import json
 from bd import response_list, users
+from jimprotocols import Client
+
 
 class MainServer:
     def __init__(self, ip, port):
@@ -12,23 +14,27 @@ class MainServer:
         self.json_data = str()
         self.string = str()
         self.sock = str()
+        self.clients = list()
+        self.chat = Chat()
 
     def connect(self):
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((self.ip, self.port))
-        self.sock.listen()
+        self.sock.listen(15)
         self.run()
 
     def run(self):
         while True:
-            #try:
             self.client, self.addr = self.sock.accept()
+            self.clients.append(self.client)
+
             try:
+                #print(self.client)
                 self.data = self.client.recv(1024)
                 self.json_data = json.loads(self.data.decode('utf-8'))
                 self.respons_generator()
-            except:
+            except IndentationError:
                 print('Error 400. Wrong JSON-object.')
                 self.run()
 
@@ -82,25 +88,74 @@ class MainServer:
     def conversation(self):
         #Здесь будет реализовано общение между пользователями, однажды...
         print('Здесь будет реализовано общение между пользователями, однажды...')
-        msg_response = {
-            "action": "msg",
-            "time": '<unix timestamp>',
-            "to": "#room_name",
-            "from": "account_name",
-            "message": "Hello World"
-            }
+        #self.chat = Chat()
+        try:
+            if self.json_data['mode'] == 'w':
+                self.chat.add_writing_client(self.client)
+                return
 
-        join_response = {
-            "action": "join",
-            "time": '< unixtimestamp >',
-            "room": "#room_name"
-            }
+            elif self.json_data['mode'] == 'r':
+                self.chat.add_reading_client(self.client)
+                print(self.chat.reading_clients)
+                return
 
-        leave_response = {
-            "action": "leave",
-            "time": '< unixtimestamp >',
-            "room": "#room_name"
-            }
+            else:
+                print('Неверный режим запуска {}. Режим запуска должен быть r - чтение или w - запись'.format(self.json_data['mode']))
+        except KeyError:
+            pass
+            #print('Без модуля')
+
+        #print(self.json_data['message'])
+        self.chat.chat_history.append(self.json_data)
+        #self.chat.read_requests()  # Получаем входные сообщения
+        print(self.chat.chat_history)
+        self.chat.write_requests() # Выполним отправку входящих сообщений
+
+class Chat:
+    def __init__(self):
+        self.chat_history = list()
+        self.instant_messages = list()
+        self.clients = list()
+        self.writing_clients = list()
+        self.reading_clients = list()
+
+    def add_writing_client(self, client):
+        self.writing_clients.append(client)
+
+    def add_reading_client(self, client):
+        self.reading_clients.append(client)
+
+    def read_requests(self):
+        """
+        Чтение сообщений, которые будут посылать клиенты
+        """
+        self.instant_messages = list()
+        for sock in self.writing_clients:
+            try:
+                self.data = sock.recv(1024).decode('utf-8')
+                self.chat_history.append(self.data)
+                self.instant_messages.append(self.data)
+            except:
+                print('Какие-то проблемы при чтении сообщений отправленных в чат')
+                #Реализовать отключение клиента TODO
+
+    def write_requests(self):
+        """
+        Отправка сообщений тем клиентам, которые их ждут
+        """
+        for sock in self.reading_clients:
+        #    for message in self.chat_history:
+            message = self.chat_history[-1]
+            try:
+                message = json.dumps(message)
+                resp = message.encode('utf-8')
+                sock.send(resp)
+
+            except ConnectionResetError:
+                print('Удаленный хост принудительно разорвал существующее подключение')
+
+                #    print('Какие-то проблемы при записи сообщений отправленных в чат')
+                    # Реализовать отключение клиента
 
 if __name__ == '__main__':
 
